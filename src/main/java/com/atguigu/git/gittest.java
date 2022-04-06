43e8ad9941157ae746dc3c03f3ec8a7ad4b6d31c
yuzhichao@qq.com <LarryYu@126.com>
2022-04-06 15:37:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gittest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;/**
+ * @ClassName gittest
+ * @Description TODO
+ * @Author hide on bush
+ * @Date Create in 2022/4/6 15:34
+ * @Version 1.0
+ */
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+    }
+}
